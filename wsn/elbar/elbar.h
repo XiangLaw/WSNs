282cc5160119888ec31cd5bd14a244b6610864f7
Vu Quoc Huy <huyvq_c633@live.com>
2015-02-15 15:57:29 +0700

    add region_0; still go to anchor point even meet a point inside grid bound hole

--- wsn/elbar/elbar.h
+++ wsn/elbar/elbar.h
@@ -10,19 +10,10 @@
 #define GREEDY_MODE 0x02
 
 enum Elbar_Region {
+    REGION_0 = 1, // inside grid boundhole
     REGION_1, // broadcast boundhole information only
     REGION_2, // broadcast boundhole information
     REGION_3, // greedy only
-};
-
-struct angleView {
-    int hole_id_;
-    double angle_;
-    struct angleView *next_;
-
-    ~angleView() {
-        angle_ = 0;
-    }
 };
 
 // parallelogram P ABC
@@ -31,7 +22,6 @@ struct parallelogram {
     struct node a_;
     struct node b_;
     struct node c_;
-    struct parallelogram *next_;
 };
 
 class ElbarGridOfflineAgent;
@@ -52,41 +42,32 @@ private:
     // gpsr
     void sendGPSR(Packet *p);
     node* recvGPSR(Packet *p, Point destination);
-    neighbor* getNeighborByPerimeter(Point);
-    neighbor* getNeighborByGreedy(Point d, Point s);
-    neighbor* getNeighborByGreedy(Point d) { return getNeighborByGreedy(d, *this); }
 
-    void broadcastHci();            // hole core information broadcast
-    void recvHci(Packet *p);        // recv hole core information
-    void routing(Packet *p);        // elbar routing algorithm
-
-    virtual char const *getAgentName();
-    void dumpParallelogram();
-    void dumpAngle();
-    void dumpNodeInfoX();
-    void dumpNodeInfoY();
-
-    void createGrid(Packet *pPacket); // re-create grid bound hole
+    void createGrid(Packet *pPacket);   // re-create grid bound hole
+    void broadcastHci();                // hole core information broadcast
+    void recvHci(Packet *p);            // recv hole core information
+    void routing(Packet *p);            // elbar routing algorithm
 
     double alpha_max_;
     double alpha_min_;
-    angleView *alpha_;          // alpha angle
-    parallelogram *parallelogram_;
+    double alpha_;          // alpha angle
+    parallelogram* parallelogram_;
     int routing_mode_;
     Elbar_Region region_;   // region to a specific hole
                             // convert to struct array for multi hole
 
-    bool isInsideGrid_;
 
+    /* math */
     bool isIntersectWithHole(Point *anchor, Point *dest, node* node_list);
     bool isAlphaContainsPoint(Point *x, Point *o, Point *y, Point *d); // check if D is inside xOy
     bool isPointLiesInTriangle(Point *d, Point *a, Point *b, Point *c);
-
     bool isPointInsidePolygon(Point *d, node *hole);
-
-
     bool isBetweenAngle(Point *pPoint, Point *pNode, Point *pMid, Point *pNode1);
 
+    /* dump */
+    virtual char const *getAgentName();
+    void dumpParallelogram();
+    void dumpAngle();
 
 protected:
     virtual void initTraceFile();
