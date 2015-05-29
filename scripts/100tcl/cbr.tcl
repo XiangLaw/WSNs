77c13466f7efa926d19f8ea9b9d6f44779d77685
Vu Quoc Huy <huyvq_c633@live.com>
2015-05-29 12:41:20 +0700

    update gitignore

---
+++ scripts/100tcl/cbr.tcl
@@ -0,0 +1,29 @@
+# udp data
+
+set opt(tn) 1
+set opt(interval) 50.0
+
+set s(0)	82	;	set d(0)	44
+
+for {set i 0} {$i < $opt(tn)} {incr i} {
+	$mnode_($s($i)) setdest [$mnode_($d($i)) set X_] [$mnode_($d($i)) set Y_] 0
+
+	set sink_($i) [new Agent/Null]
+	set udp_($i) [new Agent/UDP]	
+	$ns_ attach-agent $mnode_($d($i)) $sink_($i)
+	$ns_ attach-agent $mnode_($s($i)) $udp_($i)
+	$ns_ connect $udp_($i) $sink_($i)
+	$udp_($i) set fid_ 2
+
+	#Setup a CBR over UDP connection
+	set cbr_($i) [new Application/Traffic/CBR]
+	$cbr_($i) attach-agent $udp_($i)
+	$cbr_($i) set type_ CBR
+	$cbr_($i) set packet_size_ 50
+	$cbr_($i) set rate_ 0.1Mb
+	$cbr_($i) set interval_ $opt(interval)
+	#$cbr set random_ false
+
+	$ns_ at [expr 100 + [expr $i - 1] * $opt(interval) / $opt(tn)] "$cbr_($i) start"
+	$ns_ at [expr $opt(stop) - 5] "$cbr_($i) stop"
+}
